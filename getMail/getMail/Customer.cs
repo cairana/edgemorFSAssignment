a7c5e4867fb86d10d518ca21a5e6ffb214438d8a
cairana <ljakobsen@ymail.com>
2018-06-06 08:46:36 +0200

    Assignment060618
    Edgemo

---
+++ getMail/getMail/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getMail
+{
+    class Customer
+    {
+
+        private int _cID;
+        private string _name;
+        public string Team;
+        private string _email;
+        public int Cid
+        {
+            get { return _cID; }
+            set { _cID = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public Customer(string email)
+        {
+            Email = email;
+        }
+
+     
+
+
+    }
+
+}
+
---
+++ getMail/getMail/correspondence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getMail
+{
+    class Correspondence
+    {
+        public int id { get; set; }
+        public string subject { get; set; }
+        public DateTime time_stamp { get; set; }
+        public string mail_content { get; set; }
+        public int ticket_id { get; set; }
+
+    }
+}
